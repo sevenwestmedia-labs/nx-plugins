1e3e21d399ca6ab7f32a05948079f4160435173d
Ryan <ryan.berry@wanews.com.au>
2021-06-08 09:46:34 +0800

    use the right syntax instead

--- libs/nx-vite/src/executors/serve/executor.ts
+++ libs/nx-vite/src/executors/serve/executor.ts
@@ -1,4 +1,5 @@
 import { ExecutorContext } from '@nrwl/devkit'
+import { detectPackageManager } from '@nrwl/tao/src/shared/package-manager'
 import { FsTree } from '@nrwl/tao/src/shared/tree'
 import execa from 'execa'
 import { ServeExecutorSchema } from './schema'
@@ -10,12 +11,17 @@ export default async function runExecutor(
     if (!context.projectName) {
         throw new Error('No projectName')
     }
+    const packageManager = detectPackageManager()
     const tree = new FsTree(context.cwd, context.isVerbose)
     const appRoot = context.workspace.projects[context.projectName].root
 
-    const vite = execa(`vite`, [`${tree.root}/${appRoot}`, '--open'], {
-        stdio: [process.stdin, process.stdout, 'pipe'],
-    })
+    const vite = execa(
+        packageManager,
+        ['vite', `${tree.root}/${appRoot}`, '--open'],
+        {
+            stdio: [process.stdin, process.stdout, 'pipe'],
+        },
+    )
 
     await vite
 
