fa04d40aa4ef3683983172dbd7ba76e8a13e1471
Jake Ginnivan <jake@ginnivan.net>
2021-07-29 07:36:13 +0800

    Add standalone infrastructure project (#30)

---
+++ libs/pulumi/src/generators/init-standalone/files/src/index.ts__template__
@@ -0,0 +1,3 @@
+import * as pulumi from "@pulumi/pulumi"
+import * as aws from "@pulumi/aws"
+
---
+++ libs/pulumi/src/generators/init-standalone/schema.d.ts
@@ -0,0 +1,5 @@
+export interface PulumiGeneratorSchema {
+    name: string
+    tags?: string
+    directory?: string
+}
