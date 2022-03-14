30928777dc2ea63840c1121f7748b7fe3358a3e0
Jake Ginnivan <jake@ginnivan.net>
2022-03-14 13:31:07 +0800

    Add hook to run a command before creating zip file (#66)

---
+++ .changeset/dirty-falcons-boil.md
@@ -0,0 +1,5 @@
+---
+'@wanews/nx-esbuild': minor
+---
+
+Add beforeZip command hook for pacakge executor
--- libs/nx-esbuild/src/executors/package/schema.d.ts
+++ libs/nx-esbuild/src/executors/package/schema.d.ts
@@ -1,4 +1,5 @@
 import { BuildExecutorSchema } from '../build/schema'
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface PackageExecutorSchema extends BuildExecutorSchema {}
+export interface PackageExecutorSchema extends BuildExecutorSchema {
+    beforeZip?: string
+}
