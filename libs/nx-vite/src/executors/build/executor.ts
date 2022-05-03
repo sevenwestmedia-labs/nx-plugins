bd53bc77588e10d68ab97249fcccd01164b9de5e
BODIAN Bacary Bruno <bacarybruno@gmail.com>
2022-05-03 21:58:11 +0200

    feat(nx-vite): add outputPath

--- libs/nx-vite/src/executors/build/executor.ts
+++ libs/nx-vite/src/executors/build/executor.ts
@@ -1,5 +1,6 @@
 import { ExecutorContext } from '@nrwl/devkit'
 import { FsTree } from 'nx/src/config/tree'
+import { join } from 'path'
 import { build } from 'vite'
 import { BuildExecutorSchema } from './schema'
 
@@ -12,9 +13,15 @@ export default async function runExecutor(
     }
     const tree = new FsTree(context.cwd, context.isVerbose)
     const appRoot = context.workspace.projects[context.projectName].root
+    const outputPath = _options.outputPath
+        ? join(context.root, _options.outputPath)
+        : 'dist'
 
     await build({
         root: tree.root + '/' + appRoot,
+        build: {
+            outDir: outputPath,
+        },
     })
 
     return {
--- libs/nx-vite/src/executors/build/schema.d.ts
+++ libs/nx-vite/src/executors/build/schema.d.ts
@@ -1,2 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface BuildExecutorSchema {}
+export interface BuildExecutorSchema {
+    outputPath?: string
+}
