8f14fee6102154d2bccdd6099a87e480de508682
Bruno Bodian <bacarybruno@gmail.com>
2022-05-08 15:02:37 +0200

    refactor(nx-vite): remove fstree (#77)

--- libs/nx-vite/src/executors/build/executor.ts
+++ libs/nx-vite/src/executors/build/executor.ts
@@ -1,7 +1,6 @@
 import { ExecutorContext } from '@nrwl/devkit'
-import { FsTree } from 'nx/src/config/tree'
-import { join } from 'path'
-import { build } from 'vite'
+import * as path from 'path'
+import { build, InlineConfig } from 'vite'
 import { BuildExecutorSchema } from './schema'
 
 export default async function runExecutor(
@@ -12,18 +11,19 @@ export default async function runExecutor(
         throw new Error('No projectName')
     }
 
-    const tree = new FsTree(context.cwd, context.isVerbose)
     const appRoot = context.workspace.projects[context.projectName].root
-    const outputPath = _options?.outputPath
-        ? join(context.root, _options.outputPath)
-        : undefined
 
-    await build({
-        root: tree.root + '/' + appRoot,
-        build: {
-            outDir: outputPath,
-        },
-    })
+    const buildConfig: InlineConfig = {
+        root: context.cwd + '/' + appRoot,
+    }
+
+    if (_options?.outputPath) {
+        buildConfig.build = {
+            outDir: path.join(context.cwd, _options.outputPath),
+        }
+    }
+
+    await build(buildConfig)
 
     return {
         success: true,
