505cac47a928c600e78745c88cd96353a635f8d6
Bruno Bodian <bacarybruno@gmail.com>
2022-05-08 13:24:31 +0200

    feat(nx-vite): add outputPath (#74)

--- libs/nx-vite/src/executors/build/executor.ts
+++ libs/nx-vite/src/executors/build/executor.ts
@@ -1,4 +1,6 @@
 import { ExecutorContext } from '@nrwl/devkit'
+import { FsTree } from 'nx/src/config/tree'
+import { join } from 'path'
 import { build } from 'vite'
 import { BuildExecutorSchema } from './schema'
 
@@ -9,10 +11,18 @@ export default async function runExecutor(
     if (!context.projectName) {
         throw new Error('No projectName')
     }
+
+    const tree = new FsTree(context.cwd, context.isVerbose)
     const appRoot = context.workspace.projects[context.projectName].root
+    const outputPath = _options?.outputPath
+        ? join(context.root, _options.outputPath)
+        : undefined
 
     await build({
-        root: context.cwd + '/' + appRoot,
+        root: tree.root + '/' + appRoot,
+        build: {
+            outDir: outputPath,
+        },
     })
 
     return {
