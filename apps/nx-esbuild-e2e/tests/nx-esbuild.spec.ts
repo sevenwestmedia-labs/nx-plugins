b921731913d03cce84a98942358519157c4bd730
Charles <charles.cullen@wanews.com.au>
2024-06-10 16:20:58 +0800

    Fixed up the nx-esbuild e2e test

--- apps/nx-esbuild-e2e/tests/nx-esbuild.spec.ts
+++ apps/nx-esbuild-e2e/tests/nx-esbuild.spec.ts
@@ -9,11 +9,25 @@ import { describe, it } from 'vitest'
 
 describe('nx-esbuild e2e', () => {
     it('should create nx-esbuild', async () => {
+        console.debug(`1) Beginning to build test variables...`)
+
         const plugin = uniq('nx-esbuild')
+
+        console.debug(`2) Test variables built, let's ensure nx project...`)
+
         ensureNxProject('@wanews/nx-esbuild', 'libs/nx-esbuild')
         await runCommandAsyncHandlingError('npm install')
+
+        console.debug(
+            `3) Now let's npm install and add esbuild, nodemon & dotenv as dev dependencies...`,
+        )
+
         await runCommandAsyncHandlingError(
             'npm add esbuild nodemon dotenv --dev',
+        )
+
+        console.debug(
+            `4) Now let's generate a new nx-esbuild:node project with the id of: '${plugin}'...`,
         )
 
         await runNxCommandAsync(`generate @wanews/nx-esbuild:node ${plugin}`)
@@ -22,7 +36,7 @@ describe('nx-esbuild e2e', () => {
         // Ensure bundle exists on disk
         await new Promise((resolve) => setTimeout(resolve, 100))
 
-        checkFilesExist(`apps/${plugin}/dist/bundle.js`)
+        checkFilesExist(`${plugin}/dist/bundle.js`)
     }, 60000)
 })
 
